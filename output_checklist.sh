be2ace93e4eeb8e51cc2af7e61691653f3342341
Go Watanabe <gowatana@vmtn.jp>
2022-09-03 15:23:58 +0900

    fix checklist

--- output_checklist.sh
+++ output_checklist.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 OUTPUT_FILE=checklist.md
-echo 'watch | fav | Session | Title | Speakers' > $OUTPUT_FILE
-echo '--- | --- | ---|---|---' >> $OUTPUT_FILE
+echo '| watch | fav | Session | Title | Speakers |' > $OUTPUT_FILE
+echo '| --- | --- | --- | --- | --- |' >> $OUTPUT_FILE
 cat README.md | grep '|' | grep  -v -e '^Session' -e '^---' | while read L
 do
-  echo '[] | [] | '$L >> checklist.md >> $OUTPUT_FILE
+  echo '| <ul><li>[ ] </li></ul> | <ul><li>[ ] </li></ul> | '$L >> checklist.md >> $OUTPUT_FILE
 done
 
